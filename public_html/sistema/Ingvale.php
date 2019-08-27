a5d9d9798585a95c5d9451213854d03dacd794e9
simonreyes <sreyes707@gmail.com>
2019-08-27 15:26:37 -0400

    Nuevo crear vale

--- public_html/sistema/Ingvale.php
+++ public_html/sistema/Ingvale.php
@@ -1,113 +1,190 @@
 <?php 
-// esta cotizacion no lleva editar ni borrar, 
-// llama a add.php para insertar un item
-// llama a pdf.php para cerrar y crear la cotización
-
 define('HOST_DB', 'localhost'); 
 define('USER_DB', 'root'); 
 define('PASS_DB', ''); 
 define('NAME_DB', 'nikovald_aridos'); 
-
 function conectar()
-{ global $conexion; 
+{global $conexion; 
 //Definición global para poder utilizar en todo el contexto 
 $conexion = mysql_connect(HOST_DB, USER_DB, PASS_DB) or die ('NO SE HA PODIDO CONECTAR AL MOTOR DE LA BASE DE DATOS'); 
 mysql_select_db(NAME_DB) or die ('NO SE ENCUENTRA LA BASE DE DATOS ' . NAME_DB); 
 } function desconectar(){ global $conexion; mysql_close($conexion); }  
 
-$texto = '';
-$NombreCli = ''; 
-$Dire = ''; 
-$Giro = ''; 
-$Obra = ''; 
-$Contacto = ''; 
-$Correo = '';
-$Fono = '';
-$Rut = '';
-$Id = '';
-$registros = ''; 
+$DireccionClie = "";
+$RutClie = "";
+$Giro= ""; 
+$Obra = "";
+$ContactoClie = "";
+$CorreoClie= "";
+$FonoClie = "";
+$FolioGuia = "";
+$Destino = "";
+$Nom_Cli = "";
+$Formapago = "";
+$Transporte = "";
+$Patente = "";
+$Chofer = "";
+$Fonochofer = "";
+$Disponoble = "";
 
-$numero = "";
-$folio = "";
+$NombreCli = "";
+$Cubo = "";  
+$Tarifa = ""; 
+$Arido = ""; 
+$Valor = ""; 
+$Venta = "";
+$fonocontacto="";
 
-if($_POST){  
+if($_POST){ 
+
 $busqueda = trim($_POST['buscar']);  
 $entero = 0;  
 
-
 if (empty($busqueda))
-{ 
+{
 $texto = 'Búsqueda sin resultados'; 
 }
-else
-{ 
+else { 
+
+// Select Trae Datos de idvale
+conectar(); mysql_set_charset('utf8'); 
+$sqlv = "SELECT MAX(idvale) as maximo FROM vale";  
+$idval = mysql_query($sqlv);
+$resultadov = mysql_fetch_assoc($idval);
+$idvale = $resultadov["maximo"]+ "1"; 
+
+
 // Si hay información para buscar, abrimos la conexión
 conectar(); mysql_set_charset('utf8'); 
-$sql = "SELECT * FROM cliente WHERE idCliente = '" .$busqueda. "'";  
+$sql = "SELECT * FROM cotizacion WHERE folio = '" .$busqueda. "'";  
 $resultado = mysql_query($sql); 
-
 if (mysql_num_rows($resultado) > 0){  
 $registros = '<p>HEMOS ENCONTRADO ' . mysql_num_rows($resultado) . ' registros </p>'; 
 // Se almacenan las cadenas de resultado 
 while($fila = mysql_fetch_assoc($resultado))
 {  
-$texto .= $fila['Rut'];
-$NombreCli .= trim($fila['Nombre_Cliente']);
-$Dire .= $fila['Direccion']; 
-$Giro .= $fila['Giro'];  
-$Obra .= $fila['Obra']; 
-$Contacto .= $fila['Contacto']; 
-$Correo .= $fila['Correo']; 
-$Fono .= $fila['Fono']; 
-$Rut .= $fila['Rut'];
-$Id .= $fila['idCliente'];
+        $idguia = "";
+		$proveedor = "";
+		$km = "";
+		$cubo = "";
+		$tarifa = "";
+		$valorarido = "";
+		$ganancia = "";
+		$idaridos = "";
+		$valor = "";
+		$peaje = "";
+		$venta = "";
+		$status = "";		
+		$folio = "";
+		$idcliente = "";
+		$comision = "";
+		$ventafinal = "";
+		$Formapago = "";
+		$cantidad = "";
+		$IdObra = "";
+		$sucursal = "";
+
+	    $idguia = $fila['id_user'];
+		$proveedor = $fila['Proveedor'];
+		$km = $fila['Km'];
+		$cubo = $fila['Cubo'];
+		$tarifa = $fila['Tarifa'];
+		$valorarido = $fila['ValorArido'];
+		$ganancia = $fila['Ganancia'];
+		$idaridos = $fila['idAridos'];
+		$valor = $fila['Valor'];
+		$peaje = $fila['Peaje'];
+		$venta = $fila['Venta'];
+		$status .= $fila['Status'];		
+		$folio = $fila['Folio'];
+		$idcliente = $fila['idCliente'];
+		$comision = $fila['Comision'];
+		$ventafinal = $fila['VentaFinal'];
+		$Formapago .= $fila['FormaPago'];
+		$cantidad = '0';
+		$IdObra = $fila['idobra'];
+		$sucursal = $fila['sucursal'];
+		
+		
+		
+	mysql_query("insert into Guia_temp( id_user, Proveedor, Km, Cubo, Tarifa, ValorArido, Ganancia, idAridos, Valor, Peaje, Venta, Status, Folio, 
+	                               Fecha, IdCliente, Comision, VentaFinal, FormaPago, Cantidad, idobra, sucursal)
+						VALUES('".$idguia."', '".$proveedor."', '".$km."', '".$cubo."', '".$tarifa."', '".$valorarido."', '".$ganancia."', '".$idaridos."',
+								'".$valor."', '".$peaje."', '".$venta."', 'Creada', '".$folio."', '0', '".$idcliente."', '".$comision."', '".$ventafinal."',
+								'".$Formapago."', '".$cantidad."', '".$IdObra."', '".$sucursal."')") 
+	/*or die (mysql_error() . "insert into Guia_temp( id_user, Proveedor, Km, Cubo, Tarifa, ValorArido, Ganancia, idAridos, Valor, Peaje, Venta, Status, Folio, 
+	                               Fecha, IdCliente, Comision, VentaFinal, FormaPago, Cantidad, idobra, sucursal)
+						VALUES('".$idguia."', '".$proveedor."', '".$km."', '".$cubo."', '".$tarifa."', '".$valorarido."', '".$ganancia."', '".$idaridos."',
+								'".$valor."', '".$peaje."', '".$venta."', 'Creada', '".$folio."', '0', '".$idcliente."', '".$comision."', '".$ventafinal."',
+								'".$Formapago."', '".$cantidad."', '".$IdObra."', '".$sucursal."')")*/; 
+								}  
+
+conectar(); mysql_set_charset('utf8'); 
+$sqla = "SELECT * FROM clienteobras WHERE idobra  =  '" .$IdObra. "'";  
+$resultadoa = mysql_query($sqla) or die (mysql_error());
+//echo $resultadoa + "resultad";
+$Cnombreobra = "";
+$Ccontacto = "";
+$Cfono = "";
+$Cemail = ""; 
+while($filaa = mysql_fetch_assoc($resultadoa))
+{
+$Cnombreobra .= $filaa['nombreobra'];
+$Ccontacto .= $filaa['contacto'];
+$Cfono .= $filaa['fono'];
+$Cemail .= $filaa['email'];
 }  
-}else{ $texto = "NO HAY RESULTADOS EN LA BBDD";	 } 
-mysql_close($conexion); 
-} 
-}
 
+// Select Trae Datos Cliente
 
-$link = mysql_connect('localhost', 'root', '')
-or die('No se pudo conectar: ' . mysql_error());
-mysql_select_db('nikovald_aridos') or die('No se pudo seleccionar la base de datos'); 
- 
-$sql2 = "SELECT numero FROM folios where letra = 'CT00'";  
+conectar(); mysql_set_charset('utf8'); 
+$sql2 = "SELECT * FROM cliente WHERE idCliente = '" .$idcliente. "'";  
 $resultado2 = mysql_query($sql2); 
-while ($row = mysql_fetch_array($resultado2, MYSQL_ASSOC)) {
- 
-$numero = $row['numero'];
-} 
- 
-$folio = "CT00" . $numero; 
- 
-?> 
+while($fila2 = mysql_fetch_assoc($resultado2))
+{  
+$Nom_Cli .= $fila2['Nombre_Cliente'];
+$DireccionClie .= $fila2['Direccion'];
+$RutClie .= $fila2['Rut'];  
+$Giro .= $fila2['Giro']; 
+$ContactoClie .= $fila2['Contacto']; 
+$CorreoClie .= $fila2['Correo'];
+$FonoClie .= $fila2['Fono'];
+}  
 
+conectar(); mysql_set_charset('utf8'); 
+$sql = "SELECT * FROM folios WHERE letra  =  'GD00'";  
+$resultado = mysql_query($sql); 
+while($fila2 = mysql_fetch_assoc($resultado))
+{  
+$FolioGuia .= "GD00" . $fila2['numero'];
+}
+$Disponoble = "Guía :" . "  " . $status ;
+}else{ $texto = "NO HAY RESULTADOS EN LA BBDD";	 
+       $Disponoble = "Guía No Disponible";
+	   } 
+mysql_close($conexion); } 
+}
+?> 
 <html>
 	<head>
 	<META http-equiv=Content-Type content="text/html; charset=utf-8">
-		<title>Cotización</title>
-		<style type="text/css">
-			
+		<title>Crear Vale</title>
+		<style type="text/css">			
 			* {
 				margin:0px;
 				padding:0px;
-			}
-			
+			}			
 			#header {
 				margin:auto;
 				width:900px;
 				font-family:Arial, Helvetica, sans-serif;
-			}
-			
+			}			
 			ul, ol {
 				list-style:none;
 			}
-			
-			.nav > li {
+		    .nav > li {
 				float:left;
 			}
-			
 			.nav li a {
 				background-color:#999;
 				color:#fff;
@@ -115,54 +192,107 @@ $folio = "CT00" . $numero;
 				padding:10px 12px;
 				display:block;
 			}
-			
 			.nav li a:hover {
 				background-color:#404CF2;
 			}
-			
 			.nav li ul {
 				display:none;
 				position:absolute;
 				min-width:140px;
 			}
-			
 			.nav li:hover > ul {
 				display:block;
 			}
-			
 			.nav li ul li {
 				position:relative;
 			}
-			
 			.nav li ul li ul {
 				right:-140px;
 				top:0px;
 			}
-			
-			
 		</style>
-		<script>function formulario(f) { 
-		if (f.nombre.value   == '') 
-		{ alert ('El nombre del Cliente esta vacío');  
-		f.nombre.focus(); 
-		return false; } 
-		if (f.direccion.value  == '') 
-		{ alert ('Debe ingresar Direccion'); 
-		f.direccion.focus(); 
-		return false; } 
-		return true; } 
-		</script>
-		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	cargar_paises();
+	$("#pais").change(function(){dependencia_estado();});
+	$("#estado").change(function(){dependencia_ciudad();});
+	$("#estado").attr("disabled",true);
+	$("#ciudad").attr("disabled",true);
+});
+
+function cargar_paises()
+{
+	$.get("scripts/cargar-paises.php", function(resultado){
+		if(resultado == false)
+		{
+			alert("Error");
+		}
+		else
+		{
+			$('#pais').append(resultado);			
+		}
+	});	
+}
+function dependencia_estado()
+{
+	var code = $("#pais").val();
+	$.get("scripts/dependencia-estado.php", { code: code },
+		function(resultado)
+		{
+			if(resultado == false)
+			{
+				alert("Error");
+			}
+			else
+			{
+				$("#estado").attr("disabled",false);
+				document.getElementById("estado").options.length=1;
+				$('#estado').append(resultado);			
+			}
+		}
+
+	);
+}
+
+function dependencia_ciudad()
+{
+	var code = $("#estado").val();
+	$.get("scripts/dependencia-ciudades.php?", { code: code }, function(resultado){
+		if(resultado == false)
+		{
+			alert("Error");
+		}
+		else
+		{
+			$("#ciudad").attr("disabled",false);
+			document.getElementById("ciudad").options.length=1;
+			$('#ciudad').append(resultado);			
+		}
+	});	
+	
+}
+</script>
+<style type="text/css">
+dt{font-size:200%;}
+dd{font-size:150%;}
+</style>
+
 	</head>
-	<body>
-		<div id="header">
-			<ul class="nav">
-			</ul>
-			<div id ="block"></div>
+<body>
+<div id="header">
+	<ul class="nav">
+	</ul>			
+<div id ="block"></div>
    <div class="container">
-   <br>
-	<p style="font-size:28px;font-family:Arial, Helvetica, sans-serif;"><font color="Blue"><b>TRANSPORTES EDGARDO CRISTIAN MORENO SOLIS E.I.R.L </b></font></p>
- <h3 class="title">Crear Vale</h3>
+    <br>
+   	<p style="font-size:28px;font-family:Arial, Helvetica, sans-serif;"><font color="Blue"><b>TRANSPORTES EDGARDO CRISTIAN MORENO SOLIS E.I.R.L </b></font></p>
+        <h3 class="title">Crear Vale&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3>
 	    <div id="popupbox"></div>
         <div id="content"></div>
     </div>
@@ -175,189 +305,111 @@ $folio = "CT00" . $numero;
     <link rel="stylesheet" type="text/css" href="resources/screen.css" />
     <!-- incluyo mis estilos css -->
     <link rel="stylesheet" type="text/css" href="resources/style.css" />
-<form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>"> 
-<table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
-	<tbody>
-		<tr>
-			<td>Cliente :</td>
-			<td>
-			<?php
-				include 'conexion_mysqli.php';
-				$query = 'SELECT * FROM cliente order by Nombre_Cliente asc';
-				$result = $conexion->query($query);
-			?>
-				<select name="buscar" id="buscar">
-				<?php    
-				while ( $row = $result->fetch_array() )    
-				{
-				?>
-				<option value=" <?php echo $row['idCliente'] ?> " >
-				<?php echo $row['Nombre_Cliente']; ?>
-				</option>
-				<?php
-				}    
-				?>
-				</select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			<input type="submit" name="buscador" class="boton peque aceptar" value="Buscar"></td>
-			<td>&nbsp;</td>
-			<td>Folio :</td>
-			<td><input size="30" name="folio" type="folio" value="<?php echo $folio; ?>" /></td>
-		</tr>
 		
-	</tbody>
-</table> 
-</form>
- 
-<form action="pdf.php" method="post" target="_blank">
-<input type="hidden" name="folio" value="<?php echo $folio; ?>">			
-<table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
-	<tbody>
-		<tr style="display:none">
-			<td width="20%">id</td>
-			<td width="30%"><input size="30" name="id" type="text" value="<?php echo $Id; ?>" /></td>
-			<td width="10%">&nbsp;</td>
-			<td width="10%">&nbsp;</td>
-			<td width="30%">&nbsp;</td>
-		</tr>
+<form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>"> 
+   <table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
+	 <tbody>
 		<tr>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
+			<td>Ingrese N° Cotización : <input id="buscar" name="buscar" type="search">
+			<input type="submit" name="buscador" class="boton peque aceptar" value="Buscar">
+			</td>
+			<td>N° Vale : <?php echo $idvale ?></td>				
+		</tr>
+	</tbody>	
+ </table>
+</form>	
+ <br/>
+<form action="CancelaInserG.php" method="post" target="_blank" > 
+   <table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
+	 <tbody>
+	    <tr>
+			<td align="right"><font color="red">Cotización N°:</font>
+			</td> 
+			<td> 
+			<?php 
+		if($_POST){ 
+		$busqueda = trim($_POST['buscar']);  
+		?> 
+		<input readonly="readonly" name="busqueda" type="text"  value="<?php echo $busqueda; ?>"/>
+		<?php 		
+		 }  		
+		?>	
+        </td> 		
 		</tr>
 		<tr>
 			<td>Nombre Cliente :</td>
-			<td><input size="30" name="nombre" type="text" value="<?php echo $NombreCli; ?>" /></td>
+			<td><input readonly="readonly" name="NombreCli" id="NombreCli" type="text"  value="<?php echo $Nom_Cli; ?>"/></td>
 			<td>&nbsp;</td>
-			<td>Dirección :</td>
-			<td><input size="30" name="direccion" type="text" value="<?php echo $Dire; ?>" /></td>
+			<td>Giro :</td>
+			<td><input readonly="readonly" name="giro" type="text" size="30" value="<?php echo $Giro; ?>"/></td>
 		</tr>
 		<tr>
 			<td>Rut :</td>
-			<td><input size="30" name="rut" type="text"  value="<?php echo $Rut; ?>"/></td>
+			<td><input readonly="readonly" name="rut" type="text"  value="<?php echo $RutClie; ?>"/></td>
 			<td>&nbsp;</td>
-			<td>Giro :</td>
-			<td><input size="30" name="giro" type="text" value="<?php echo $Giro; ?>"/></td>
+			<td>Dirección :</td>
+			<td><input readonly="readonly" name="DireccionClie" type="text" size="30" value="<?php echo $DireccionClie; ?>"/></td>
 		</tr>
 		<tr>
-			<td>Obra :</td>
+			<td>Cuidad :</td>
+			<td><input readonly="readonly" name="rut" type="text"  value="<?php echo $RutClie; ?>"/></td>
+			<td>&nbsp;</td>
+			<td>Cantidad :</td>
+			<td><input readonly="readonly" name="giro" type="text" size="30" value="<?php echo $Giro; ?>"/></td>
+		</tr>
+		<tr>
+			<td>Cantidad :</td>
+			<td><input readonly="readonly" name="rut" type="text"  value="<?php echo $RutClie; ?>"/></td>
+			<td>&nbsp;</td>
+			<td>Detalle :</td>
+			<td><textarea name="comentarios" rows="3" cols="6"></textarea></td>			
+		</tr>		
+    </tbody>
+  </table>
+  <table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
+	 <tbody>
+	    <tr>
+			<td>Transporte</td> 
+			<td></td>
+		    <td>Patente</td>
+			<td></td>
+			<td>Chofer</td>	
+			<td></td>					
+		</tr>
+		 <tr>
 			<td>
-			<?php
-				include 'conexion_mysqli.php';
-				$query = "SELECT * FROM clienteobras where nombrecliente = '".$Id."'";
-				$result = $conexion->query($query);
-			?>
-				<select name="obra" id="obra">
-				<?php    
-				while ( $row = $result->fetch_array() )    
-				{
-				?>
-				<option value=" <?php echo $row['idobra'] ?> " >
-				<?php echo $row['nombreobra']; ?>
-				</option>
-				<?php
-				}    
-				?>
-				</select>
-				</td>
-			<td>&nbsp;</td>
-			<td>Contacto :</td>
-			<td><input size="30" name="contacto" type="text" value="<?php echo $Contacto; ?>"/></td>
-		</tr>
-		<tr>
-			<td>Correo Electronico:</td>
-			<td><input size="30" name="correo" type="text" value="<?php echo $Correo; ?>"/></td>
-			<td>&nbsp;</td>
-			<td>Fono :</td>
-			<td><input size="30" name="fono" type="text" value="<?php echo $Fono; ?>"/></td>
-		</tr>
-		<tr>
-			<td>Forma de Pago :</td>
-			<td>
-				<select name="fp" >
-				<option value="Al Dia">Al Dia</option>
-				<option value="15 Dias">15 Dias</option>
-				<option value="30 Dias">30 Dias</option>
-				<option value="30 Dias">60 Dias</option>
-				</select>
-			</td>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
-			<td>&nbsp;</td>
+			<select id="pais" name="pais" >
+				<option value="0">Seleccione Transporte...</option>
+			</select></td> 
+			<td></td>
+		    <td> 
+			<select id="estado" name="estado">
+				<option value="0">Seleccione Patente...</option>
+			</select></td>
+			<td></td>
+			<td>  
+			<select id="ciudad" name="ciudad">
+				<option value="0">Seleccione Chofer...</option>
+			</select></td>	
+			<td></td>
 		</tr>
 	</tbody>
-	</table>
-	<?php
-	//including the database connection file
-	//include_once("config.php");
-
-	//fetching data in descending order (lastest entry first)
-	//$result = mysql_query("SELECT * FROM cotizacion ORDER BY id_User DESC");
-
-	mysql_connect("localhost", "root", "") or die ('NO SE HA PODIDO CONECTAR AL MOTOR DE LA BASE DE DATOS'); 
-	mysql_select_db("nikovald_aridos") or die ('NO SE ENCUENTRA LA BASE DE DATOS ' . NAME_DB); 
-
-	$result="	SELECT *
-				from cotizacion c
-				inner join aridos a on c.idaridos = a.idaridos
-				inner join proveedores p on c.proveedor = p.idproveedor
-				where c.folio = 'CT00' ";
-	$res=mysql_query($result);
-	?>
-	<a href="add.php?Vid=<?php echo $Id; ?>&Vfolio=<?php echo $folio; ?> ">Insertar Item</a><br/><br/>
-	
- 
-	<table style="width:950px;" border=0>
- 
-		<tr>
-            <th>PROVEEDOR</th>
-            <th>KM</th>
-            <th>CUBO/KM</th>
-            <th>TARIFA</th>
-			<th>VALOR ARIDO</th>
-			<th>GANANCIA</th>
-			<th>ARIDO</th>
-			<th>VALOR</th>
-			<th>PEAJE</th>
-			<th>COMISION</th>
-			<th>VENTA</th>
-			<th>VENTA FINAL</th>
-			<th>CANTIDAD</th>
-        </tr>
-	<?php 
-	while($row = mysql_fetch_array($res)) { 		
-		echo "<tr>";
-		$idcoti = $row['id_user'];
-		echo "<td>".$row['Proveedor']."</td>";
-		echo "<td>".$row['Km']."</td>";
-		echo "<td>".$row['Cubo']."</td>";
-		echo "<td>".$row['Tarifa']."</td>";
-		echo "<td>".$row['ValorArido']."</td>";
-		echo "<td>".$row['Ganancia']."</td>";
-		echo "<td>".$row['glosa']."</td>";
-		echo "<td>".$row['Valor']."</td>";
-		echo "<td>".$row['Peaje']."</td>";
-		echo "<td>".$row['Comision']."</td>";
-		echo "<td>".$row['Venta']."</td>";
-		echo "<td>".$row['VentaFinal']."</td>";
-		echo "<td>".$row['Cantidad']."</td>";			
-	}
-	?>
-	</table>
-		<table align="center" border="0" cellpadding="1" cellspacing="1" style="width:950px;" >
-	<tbody>
-		<tr>
-			<td width="20%">&nbsp;</td>
-			<td width="20%">&nbsp;</td>
-			<td width="20%"><input type="button" name="Cancelar" value="Inicio" onClick="location.href='DeleteCotiza.php'"></td>
-			<td width="20%">&nbsp;</td>
-			<td width="20%">&nbsp;</td>
-		</tr>
-	</tbody>
-	</table>
-	</form>			
-	</div>
-	
-	</body>
+  </table>
+ <br/> 
+</table>   
+ <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:950px;" >
+ <tr>
+    <td size="40">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;</td>	
+   	<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>	
+	<td><input type="submit" name="Guardarv" value=" Guardar " onClick="location.href='vistaGral.php'"></td>
+	<td><input type="submit" name="Cancelar" value=" Cancelar " onClick="location.href='vistaGral.php'"></td>	
+	<td size="40"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>	
+   <tr>
+   </table>
+  <br/>
+</form>  
+</div>
+</body>
 </html>
