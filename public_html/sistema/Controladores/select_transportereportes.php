d2a112c759a600ca00a9ecac0a58ea9f03322a70
Dansnk <snk.styles@gmail.com>
2019-12-16 11:52:45 -0300

    Reporte Transportistas - Seleccionar todo
    Ver todos los transportistas en el reporte

--- public_html/sistema/Controladores/select_transportereportes.php
+++ public_html/sistema/Controladores/select_transportereportes.php
@@ -14,14 +14,15 @@ if(!empty($_POST["FechaInicial"])){
     // Fetch state data based on the specific country 
     $query = "SELECT DISTINCT(g.Transp_Guia) as nombretransp FROM Guia as g WHERE Fecha_Guia >= '".$FechaInicial."' AND Fecha_Guia <= '".$FechaFinal."' ORDER BY nombretransp";
     $query = mysqli_query($con, $query);
-
-    $queryc = "SELECT COUNT(*) as numrows FROM Guia WHERE Fecha_Guia >= '".$FechaInicial."' AND Fecha_Guia <= '".$FechaFinal."'";
+   
+ $queryc = "SELECT COUNT(*) as numrows FROM Guia WHERE Fecha_Guia >= '".$FechaInicial."' AND Fecha_Guia <= '".$FechaFinal."'";
     $count_queryTR = mysqli_query($con, $queryc);
     if ($rowTR = mysqli_fetch_array($count_queryTR)){$numrowsTR = $rowTR['numrows'];}
     else {echo mysqli_error($con);}
     // Generate HTML of state options list 
     if($numrowsTR > 0){ 
-        echo '<option value="">Selecciona Transportista</option>'; 
+        echo '<option value="">Selecciona Transportista</option>';
+        echo '<option value="ALL">*SELECCIONA TODOS*</option>';
         while($row = mysqli_fetch_array($query)){  
             echo '<option value="'.$row['nombretransp'].'">'.$row['nombretransp'].'</option>'; 
         } 
